ead6381be2c63f448bbf6d8f2ed4200d8b7e10b1
pzajaczkowski <demoon.pps@gmail.com>
2024-11-08 13:01:37 +0100

    Create serial solution

--- solutions/serial/src/main.cpp
+++ solutions/serial/src/main.cpp
@@ -1,6 +1,28 @@
-#include <iostream>
+#include <utils.hpp>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main(const int argc, char* argv[]) {
+    // #1 Parse command-line arguments
+    Args args;
+    parseArguments(argc, argv, &args);
+
+    // #2 Initialize board
+    Board board(args.matrixSize, args.matrixSize);
+    board.Init(args.initType);
+
+    // #3 Run iterations
+    for (int i = 0; i < args.iterations; ++i) {
+        // #4 Save snapshot if verbose
+        if (args.is_verbose) {
+            PBM pbm = PBMFromBoard(board);
+            savePBM(pbm, args.outputDirectory, i);
+        }
+
+        // #5 Update board with empty ghost rows
+        board.updateBoard(nullptr, nullptr);
+    }
+
+    if (args.is_verbose) {
+        const PBM pbm = PBMFromBoard(board);
+        savePBM(pbm, args.outputDirectory, args.iterations);
+    }
 }
